f4fd48a1259be941d88a8e2e1ae9eef2f9136acb
crappycoder1 <aaryan.vijayraj@gmail.com>
2020-08-27 13:33:57 +0530

    new modifications in recievedata

--- routes/recievedata.js
+++ routes/recievedata.js
@@ -1,15 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+function processData(req) {
+    let sendValue
+    if(req && req.data) {
+        sendValue = req.data
+    } else {
+        sendValue = req
+        console.log('inside else in processData')
+    }
+    return sendValue
+}
 
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
     console.log('/recievedata endpoint hit with GET request')
     let sendValue
     try {
-
+        sendValue = processData(req)
     } catch (err) {
-        
+        sendValue = err
     }
+    res.send(sendValue)
 });
 
 module.exports = router;
