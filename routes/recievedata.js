1cdfed7812f13efdb08d78e0ff78bc859ef46cb3
crappycoder1 <aaryan.vijayraj@gmail.com>
2020-08-27 12:57:51 +0530

    modifications

---
+++ routes/recievedata.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/', function(req, res, next) {
+    console.log('/recievedata endpoint hit with GET request')
+    let sendValue
+    try {
+
+    } catch (err) {
+        
+    }
+});
+
+module.exports = router;
